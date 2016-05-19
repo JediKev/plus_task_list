6dbb122aae48157a19e9eb6e87011c1002a5e130
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-19 10:42:11 -0500

    Adds navbar and navbar links partials for facebook sign in and sign out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+	# Sign In
+	def create
+		auth = request.env['omniauth.auth']
+		# Go look in DB for user with that provider and uid. If they don't exist yet, create them.
+		user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
+		# Set session[:user_id]
+		session[:user_id] = user.id
+
+		redirect_to root_url, notice: "Welcome, #{user.name}."
+	end
+
+	# Sign Out
+	def destroy
+		session[:user_id] = nil
+
+		redirect_to root_url, notice: "You've left us! Bye Felicia!"
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+	# Create User using the JSON response returned from the Facebook API
+	def self.create_with_omniauth(auth)
+		create! do |user|
+			user.provider = auth['provider']
+			user.uid = auth['uid']
+			user.name = auth['info']['name']
+		end
+	end
+	def small_image
+		"http://graph.facebook.com/#{self.uid}/picture?type=small"
+	end
+	def normal_image
+		"http://graph.facebook.com/#{self.uid}/picture?type=normal"
+	end
 end

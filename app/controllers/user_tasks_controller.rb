770ae2f623226f1175c11f775f872adccbed577a
Kevin Thorne <kevinthorne7@yahoo.com>
2016-05-24 08:58:07 -0500

    Adds private methods to restrict users from entering things in url path

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -76,10 +76,24 @@ class UserTasksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user_task
       @user_task = UserTask.find(params[:id])
+      if current_user.id == @user_task.user_id
+        @user_task
+      else
+        invalid_task
+      end
+    end
+
+    def invalid_task
+      logger.error "Attempt to access invalid task #{params[:id]}"
+      redirect_to user_tasks_url, notice: "Invalid task. Access denied."
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_task_params
       params.require(:user_task).permit(:description, :due)
+    end
+
+    def set_all_tasks
+      @user_tasks = UserTask.where(user_id: current_user.id).order(:due)
     end
 end
